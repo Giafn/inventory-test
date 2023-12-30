e060c43e8e8a4933a1023466df16d7188410ce60
Giafn <giafauzan11@gmail.com>
2023-12-30 19:20:06 +0700

    update fix hitungan total

--- resources/views/purchase/js.blade.php
+++ resources/views/purchase/js.blade.php
@@ -110,7 +110,7 @@
                     render: function(data) {
                         let total = 0;
                         data.details.forEach((details, index) => {
-                            total += details.price * details.qty;
+                            total += details.price;
                         });
                         // format to IDR
                         return new Intl.NumberFormat('id-ID', {
